1bbfe021c34b9a2be8cf39c3dd89bedb4eab8d70
Sasha Bachynskyi <allfred12@gmail.com>
2023-10-26 15:50:57 +0300

    implemented BookController

--- src/main/java/com/onlinebookstore/service/BookServiceImpl.java
+++ src/main/java/com/onlinebookstore/service/BookServiceImpl.java
@@ -1,27 +1,39 @@
 package com.onlinebookstore.service;
 
+import com.onlinebookstore.dto.BookDto;
+import com.onlinebookstore.dto.CreateBookRequestDto;
+import com.onlinebookstore.exception.EntityNotFoundException;
+import com.onlinebookstore.mapper.BookMapper;
 import com.onlinebookstore.model.Book;
 import com.onlinebookstore.repository.BookRepository;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    @Override
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public List<BookDto> getAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public List<Book> getAll() {
-        return bookRepository.findAll();
+    public BookDto getBookById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findBookById(id);
+        return bookMapper.toDto(optionalBook.orElseThrow(EntityNotFoundException::new));
     }
 }
