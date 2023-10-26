a8d5c85298fa4865cd14883f7412e55149ce47a5
Sasha Bachynskyi <allfred12@gmail.com>
2023-10-26 15:50:57 +0300

    implemented BookController

--- src/main/java/com/onlinebookstore/exception/RepositoryAppException.java
+++
@@ -1,7 +0,0 @@
-package com.onlinebookstore.exception;
-
-public class RepositoryAppException extends RuntimeException {
-    public RepositoryAppException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
