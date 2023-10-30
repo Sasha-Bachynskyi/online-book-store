2a459315c5cec82d35758686baba372ad5279c6a
Sasha Bachynskyi <allfred12@gmail.com>
2023-10-30 19:42:39 +0200

    implemented JpaRepository, liquibase, soft delete

--- src/main/java/com/onlinebookstore/controller/BookController.java
+++ src/main/java/com/onlinebookstore/controller/BookController.java
@@ -1,15 +1,19 @@
 package com.onlinebookstore.controller;
 
 import com.onlinebookstore.dto.BookDto;
-import com.onlinebookstore.dto.CreateBookRequestDto;
+import com.onlinebookstore.dto.BookRequestDto;
 import com.onlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -19,7 +23,7 @@ public class BookController {
     private final BookService bookService;
 
     @PostMapping
-    public BookDto save(@RequestBody CreateBookRequestDto requestDto) {
+    public BookDto save(@RequestBody BookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
@@ -31,5 +35,16 @@ public class BookController {
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody BookRequestDto requestDto) {
+        bookService.update(id, requestDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
     }
 }
--- src/main/java/com/onlinebookstore/service/BookService.java
+++ src/main/java/com/onlinebookstore/service/BookService.java
@@ -1,13 +1,17 @@
 package com.onlinebookstore.service;
 
 import com.onlinebookstore.dto.BookDto;
-import com.onlinebookstore.dto.CreateBookRequestDto;
+import com.onlinebookstore.dto.BookRequestDto;
 import java.util.List;
 
 public interface BookService {
-    BookDto save(CreateBookRequestDto requestDto);
+    BookDto save(BookRequestDto requestDto);
 
     List<BookDto> getAll();
 
     BookDto getBookById(Long id);
+
+    void update(Long id, BookRequestDto requestDto);
+
+    void deleteById(Long id);
 }
