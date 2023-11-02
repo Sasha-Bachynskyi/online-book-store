eb5067a37feca93b3b7586569cc8dbadf65415de
Sasha Bachynskyi <allfred12@gmail.com>
2023-11-02 21:14:40 +0200

    implemented CustomGlobalExceptionHandler and BookRequestDto validation

--- src/main/java/com/onlinebookstore/dto/BookRequestDto.java
+++ src/main/java/com/onlinebookstore/dto/BookRequestDto.java
@@ -1,5 +1,8 @@
 package com.onlinebookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +10,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookRequestDto {
+    @NotNull
+    @NotBlank
     private String title;
+    @NotNull
+    @NotBlank
     private String author;
+    @NotNull
+    @NotBlank
     private String isbn;
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
