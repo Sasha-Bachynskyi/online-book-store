747bf393708f4b1f89da66dd7a47a6e8f4ff2f6e
Sasha Bachynskyi <allfred12@gmail.com>
2023-11-13 19:01:15 +0200

    implemented category entity

--- src/main/java/com/onlinebookstore/model/Book.java
+++ src/main/java/com/onlinebookstore/model/Book.java
@@ -2,14 +2,19 @@ package com.onlinebookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import java.util.Set;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -42,6 +47,12 @@ public class Book {
 
     @EqualsAndHashCode.Exclude
     @ToString.Exclude
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "books_categories",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
 }
