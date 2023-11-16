4560711850c5782cbd59f6c64a9c2b3b1c76fca6
Sasha Bachynskyi <allfred12@gmail.com>
2023-11-16 12:37:58 +0200

    getter setter annotations to data annotation

--- src/main/java/com/onlinebookstore/model/Book.java
+++ src/main/java/com/onlinebookstore/model/Book.java
@@ -7,14 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
-@Getter
-@Setter
+@Data
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted=false")
 @Table(name = "books")
@@ -40,6 +40,8 @@ public class Book {
     @Column(name = "cover_image")
     private String coverImage;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
 }
