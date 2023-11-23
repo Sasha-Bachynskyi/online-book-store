c4a4d95dc19c627420ad7266ee51ebfd1ccad516
Sasha Bachynskyi <allfred12@gmail.com>
2023-11-23 12:59:59 +0200

    added mapping ids in BookMapper

--- src/main/java/com/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/com/onlinebookstore/mapper/BookMapper.java
@@ -5,7 +5,13 @@ import com.onlinebookstore.dto.book.BookDto;
 import com.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
 import com.onlinebookstore.dto.book.BookRequestDto;
 import com.onlinebookstore.model.Book;
+import com.onlinebookstore.model.Category;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.springframework.util.CollectionUtils;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
@@ -14,4 +20,14 @@ public interface BookMapper {
     Book toModel(BookRequestDto requestDto);
 
     BookDtoWithoutCategoryIds toDtoWithoutCategories(Book book);
+
+    @AfterMapping
+    default void setCategoryIds(@MappingTarget BookDto bookDto, Book book) {
+        if (!CollectionUtils.isEmpty(book.getCategories())) {
+            Set<Long> categoryIds = book.getCategories().stream()
+                    .map(Category::getId)
+                    .collect(Collectors.toSet());
+            bookDto.setCategoryIds(categoryIds);
+        }
+    }
 }
