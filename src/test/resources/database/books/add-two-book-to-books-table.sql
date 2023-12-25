f436c680fe926beb4f05b90df0178574d48c264d
Sasha Bachynskyi <allfred12@gmail.com>
2023-12-25 11:14:26 +0200

    implemented book repository testing

---
+++ src/test/resources/database/books/add-two-book-to-books-table.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (id, title, author, isbn, price)
+VALUES (1, 'Harry Potter and the Philosopher''s Stone', 'J. K. Rowling', '0-061-96436-0', 10.99);
+INSERT INTO books (id, title, author, isbn, price)
+VALUES (2, 'The Great Gatsby', 'F. Scott Fitzgerald', '0-061-95636-0', 15.99);
